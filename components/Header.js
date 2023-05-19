08792c7cf5d0ae3b021b972566d2659cef6bd4c0
K K Haridev <kkharidev@gmail.com>
2023-05-20 00:02:04 +0530

    Added option to delete all todos

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,33 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, Pressable, View, Alert } from "react-native";
 
-export default function Header() {
+export default function Header({deleteAll}) {
+  
   return (
     <View style={styles.header}>
       <Text style={styles.title}>TODO APP</Text>
+      <Pressable
+        onPress={() =>
+          Alert.alert("Delete All Todo", ``, [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel Pressed"),
+              style: "cancel",
+            },
+            {
+              text: "OK",
+              onPress: deleteAll,
+            },
+          ])
+        }
+        style={({ pressed }) => [
+          {
+            borderColor: pressed ? "#FE5F55" : "#7E78D2",
+          },
+          styles.circular,
+        ]}
+      ></Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,11 +37,19 @@ const styles = StyleSheet.create({
   header: {
     marginTop: 45,
     padding: 20,
-    justifyContent:'center'
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
-    color:"#6F58C9",
+    color: "#6F58C9",
+  },
+  circular: {
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    borderRadius: 10,
   },
 });
